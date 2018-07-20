aa11645db6e6c15a3b219f526f2fe700f7b3376a
Kars Rhyder <karsrhyder@gmail.com>
2018-07-20 19:26:25 +0200

    HashtagList added
    compiles!

---
+++ contracts/HashtagProxy/HashtagList.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.4.23;
+
+/**
+*  @title Hashtag List
+*  @dev Created in Swarm City anno 2018,
+*  for the world, with love.
+*  description Hashtag List Contract
+*  description This is the Hashtag List contract for Swarm City marketplaces.
+*  In it, "allowed" marketplaces that are advertised in the app are being stored.
+*/
+
+import "../Ownable.sol";
+import "./IPFSEvents.sol";
+
+
+contract HashtagList is IPFSEvents, Ownable {
+
+    struct hashtagListItem {
+        string hashtagName;
+        string hashtagMetaIPFS;
+        address hashtagAddress;
+        bool hashtagShown;
+    } 
+
+    hashtagListItem[] hashtagListArray;
+
+    uint public defaultTTL;
+    
+    event HashtagAdded(string hashtagName, string hashtagMetaIPFS, address hashtagAddress);
+    
+    function addHashtag(string _hashtagName, string _hashtagMetaIPFS, address _hashtagAddress) onlyOwner external {
+        require(bytes(_hashtagName).length != 0);
+        uint indexHashtag = hashtagListArray.length++;
+        hashtagListItem storage c = hashtagListArray[indexHashtag];
+        c.hashtagName = _hashtagName;
+        c.hashtagMetaIPFS = _hashtagMetaIPFS;
+        c.hashtagAddress = _hashtagAddress;
+        c.hashtagShown = true; 
+        HashtagAdded(_hashtagName, _hashtagMetaIPFS, _hashtagAddress);
+    }
+
+    function readHashtag(uint _index) public returns (hashtagListItem) {
+        return hashtagListArray[_index];
+    }
+}
